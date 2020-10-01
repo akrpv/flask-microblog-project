170e803770eaf4c91d42a618d764238b336e6f22
akrpv <al.kurapov@gmail.com>
2020-10-01 07:59:03 +0300

    added models

--- app/config.py
+++ app/config.py
@@ -1,2 +1,4 @@
 class Configuration(object):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user='flask_user',pw='123',url='localhost:5432',db='flask_db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
